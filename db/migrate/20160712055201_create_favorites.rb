f6ab630136fda458dacad9620169d957c2066f09
Nick Daugherty <ndaugherty18@gmail.com>
2018-08-28 15:39:25 -0700

    fix migrations

--- db/migrate/20160712055201_create_favorites.rb
+++ db/migrate/20160712055201_create_favorites.rb
@@ -1,4 +1,4 @@
-class CreateFavorites < ActiveRecord::Migration
+class CreateFavorites < ActiveRecord::Migration[5.0]
   def change
     create_table :favorites do |t|
       t.references :user, index: true, foreign_key: true
