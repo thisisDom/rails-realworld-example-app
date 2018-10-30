b011f3fb770233a4b52da58eedcbec6708008c45
Dominique Crowther <Dominique.Crowther@gmail.com>
2018-10-29 17:11:14 -0700

    Fix Sign Up bug
    -add registration controller that inherits from Devise's registration_controller
    -use custom registration controller instead of Devise's registration_controller
    -add ability to respond to requests wanting json responses in custom registration_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
-    devise_for :users, controllers: { sessions: :sessions },
+    devise_for :users, controllers: { sessions: :sessions, registrations: :registrations },
                        path_names: { sign_in: :login }
 
     resource :user, only: %i[show update]
